478a1e974e993b531f703b60898062a308269299
dimandzhi <dimandzhi@inbox.lv>
2014-11-06 15:44:24 +0200

    Working Sony Vegas script project

---
+++ DialogWin/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+
+namespace DialogWin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double fps;
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        public MainWindow(string[] args)
+        {
+            InitializeComponent();
+            AllocConsole();
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+            if (args.Length > 0) fps = Convert.ToDouble(args[0]);
+            else fps = 29.97;
+            Time.ValueChanged += Time_ValueChanged;
+            Frames.ValueChanged += Frames_ValueChanged;
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(Frames.Value.ToString());
+            Console.WriteLine(Time.Value.ToString());
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            FreeConsole();
+        }
+
+        private void Frames_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Time.ValueChanged -= Time_ValueChanged;
+            Frames.ValueChanged -= Frames_ValueChanged;
+            Frames.Value = Math.Round(Convert.ToDouble(Frames.Value));
+            Time.Value = Frames.Value * 1000 / fps;
+            Time.Value =Math.Round(Convert.ToDouble(Time.Value), 2);
+            Time.ValueChanged += Time_ValueChanged;
+            Frames.ValueChanged += Frames_ValueChanged;
+        }
+
+        private void Time_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+           
+            Frames.ValueChanged -= Frames_ValueChanged;
+            Time.ValueChanged -= Time_ValueChanged;
+            Time.Value = Math.Round(Convert.ToDouble(Time.Value / 10)) * 10;
+            Frames.Value = Time.Value / fps;
+            Frames.Value = Math.Round(Convert.ToDouble(Frames.Value), 2);
+            Frames.ValueChanged += Frames_ValueChanged;
+            Time.ValueChanged += Time_ValueChanged;
+        }
+
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                e.Handled = true;
+            else if (e.Key.ToString().Length == 2)
+            {
+                if (!Char.IsDigit(e.Key.ToString(), 1))
+                    e.Handled = true;
+            }
+            else if (e.Key.ToString().Length == 7)
+            {
+                if (!Char.IsDigit(e.Key.ToString(), 6))
+                    e.Handled = true;
+            }
+            else e.Handled = true;
+        }
+    }
+}
---
+++ DialogWin/Overlay.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Sony.Vegas;
+using System.Windows.Forms;
+
+namespace Overlay
+{
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas vegas)
+        {
+            double frames = 0;
+            double time = 0;
+            ProcessStartInfo StartInfo = new ProcessStartInfo
+            {
+                FileName = AppDomain.CurrentDomain.BaseDirectory + @"Script Menu\DialogWin.exe",
+                Arguments = vegas.Project.Video.FrameRate.ToString(),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            Process DialgWin = Process.Start(StartInfo);
+            int i = 0;
+            while (!DialgWin.StandardOutput.EndOfStream)
+            {
+                switch (i)
+                {
+                    case 0:
+                        frames = Convert.ToDouble(DialgWin.StandardOutput.ReadLine());
+                        break;
+                    case 1:
+                        time = Convert.ToDouble(DialgWin.StandardOutput.ReadLine());
+                        break;
+                }
+                i++;
+            }
+            DialgWin.WaitForExit();
+            i = 0;
+            double timecode = 0;
+            int ind;
+            int prev_ind = -1;
+            foreach (Track track in vegas.Project.Tracks)
+            {
+                List<TrackEvent> Events = new List<TrackEvent>();
+                foreach (TrackEvent trev in track.Events) Events.Add(trev);
+                Events.Sort(new SortByTime());
+                foreach (TrackEvent trev in Events)
+                {
+                    if (trev.Selected)
+                    {
+                        ind = Events.FindIndex((TrackEvent tr) => { return tr.Start == trev.Start; });
+                        if (ind == prev_ind + 1)
+                        {
+                            if (i == 0)
+                            {
+                                timecode = trev.End.ToMilliseconds();
+                                i++;
+                            }
+                            else
+                            {
+                                trev.Start = new Timecode(timecode - time);
+                                timecode = trev.End.ToMilliseconds();
+                            }
+                        }
+                        else i = 0;
+                        prev_ind = ind;
+                    }
+                }
+                prev_ind = -1;
+                i = 0;
+            }
+            MessageBox.Show("Success!");
+        }
+
+        public class SortByTime : IComparer<TrackEvent>
+        {
+            public int Compare(TrackEvent a, TrackEvent b)
+            {
+                if (a.Start > b.Start) return 1;
+                else if (a.Start < b.Start) return -1;
+                else return 0;
+            }
+        }
+    }
+}
